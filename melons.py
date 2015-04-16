cf022716728e7fa059c36146b1cb42e1faff316f
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 12:05:38 -0700

    Initializing repository

---
+++ melons.py
@@ -0,0 +1,16 @@
+"""This file should have our melon-type classes in it."""
+
+
+class WatermelonOrder(object):
+    species = "Watermelon"
+    color = "green"
+    imported = False
+    shape = 'natural'
+    seasons = ['Fall', 'Summer']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
